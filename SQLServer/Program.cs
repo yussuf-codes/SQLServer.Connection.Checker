ac0b04f837bc4f513d4afca4fb42a04011c08c27
yussuf-codes <yussuf.code@gmail.com>
2024-02-17 04:30:29 -0800

    SQL Server Test

---
+++ SQLServer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace SQLServer;
+
+static class SqlClient
+{
+    public static DataTable GetDatabases(string connectionString)
+    {
+        DataTable databasesTable = new DataTable();
+        string selectDatabases = "SELECT database_id AS id, name FROM sys.databases;";
+        using SqlConnection sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = new SqlCommand(selectDatabases, sqlConnection);
+            sqlConnection.Open();
+            using SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                sqlDataAdapter.Fill(databasesTable);
+            sqlConnection.Close();
+        return databasesTable;
+    }
+
+    public static string GetVersion(string connectionString)
+    {
+        string selectVersion = "SELECT @@VERSION;";
+        using SqlConnection sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = new SqlCommand(selectVersion, sqlConnection);
+            sqlConnection.Open();
+            string version = (string)sqlCommand.ExecuteScalar();
+            sqlConnection.Close();
+        return version;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Server=localhost;Database=master;User Id=sa;Password=Pa$$word;TrustServerCertificate=true;";
+        DataTable databases = SqlClient.GetDatabases(connectionString);
+        for (int i = 0; i < databases.Rows.Count; i++)
+        {
+            Console.Write($"Id: {(int)databases.Rows[i]["id"]}");
+            Console.Write('\t');
+            Console.Write($"Name: {(string)databases.Rows[i]["name"]}");
+            Console.Write('\n');
+        }
+
+        string version = SqlClient.GetVersion(connectionString);
+        Console.WriteLine(version);
+    }
+}
---
+++ SQLServer/checkService.sh
@@ -0,0 +1,7 @@
+sudo systemctl status mssql-server
+
+# start service
+sudo systemctl start mssql-server
+
+# enable service
+sudo systemctl enable mssql-server
