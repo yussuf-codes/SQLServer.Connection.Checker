30a79605fc7c6644b050ec6da3380d5318d525b5
yussuf-codes <yussuf.code@gmail.com>
2024-08-12 11:49:05 +0300

    Refactoring

---
+++ src/Checker/MSSQLDataProvider.cs
@@ -0,0 +1,38 @@
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace Checker;
+
+internal class MSSQLDataProvider
+{
+    private readonly string _connectionString;
+
+    public MSSQLDataProvider(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public DataTable GetDatabases()
+    {
+        DataTable databases = new DataTable();
+        string selectDatabasesQuery = "SELECT database_id AS id, name FROM sys.databases;";
+        using SqlConnection sqlConnection = new SqlConnection(_connectionString);
+        SqlCommand sqlCommand = new SqlCommand(selectDatabasesQuery, sqlConnection);
+        sqlConnection.Open();
+        using SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+        sqlDataAdapter.Fill(databases);
+        sqlConnection.Close();
+        return databases;
+    }
+
+    public string GetVersion()
+    {
+        string selectVersionQuery = "SELECT @@VERSION;";
+        using SqlConnection sqlConnection = new SqlConnection(_connectionString);
+        SqlCommand sqlCommand = new SqlCommand(selectVersionQuery, sqlConnection);
+        sqlConnection.Open();
+        string version = (string)sqlCommand.ExecuteScalar();
+        sqlConnection.Close();
+        return version;
+    }
+}
---
+++ src/Checker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+
+namespace Checker;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        string connectionString = args[0];
+
+        MSSQLDataProvider provider = new MSSQLDataProvider(connectionString);
+
+        DataTable databases = provider.GetDatabases();
+
+        for (int i = 0; i < databases.Rows.Count; i++)
+        {
+            System.Console.Write($"Id: {(int)databases.Rows[i]["id"]}");
+            System.Console.Write('\t');
+            System.Console.Write($"Name: {(string)databases.Rows[i]["name"]}");
+            System.Console.Write(System.Environment.NewLine);
+        }
+
+        string version = provider.GetVersion();
+
+        System.Console.WriteLine(version);
+    }
+}
